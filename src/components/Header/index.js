3c571ff7c5d87d245c0ee795c7fcdb8d572e963d
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-04-02 20:23:17 -0300

    Create header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './styles.css';
+import { Link } from 'gatsby';
 import { isMobile } from 'react-device-detect';
+import './styles.css';
 
 import menuIcon from '../../images/menuIcon.svg';
 import hawkLogo from '../../images/hawkIcon.svg';
@@ -9,10 +10,36 @@ import cartIcon from '../../images/cartIcon.svg';
 import searchIcon from '../../images/searchIcon.svg';
 
 export default function Header() {
+  const state = {
+    navbarOpen: false,
+    links: [
+      {
+        id: 1,
+        path: '/camisetas',
+        text: 'camisetas'
+      },
+      {
+        id: 2,
+        path: '/shorts',
+        text: 'shorts'
+      },
+      {
+        id: 3,
+        path: '/calças',
+        text: 'calças'
+      },
+      {
+        id: 4,
+        path: '/acessorios',
+        text: 'acessórios'
+      }
+    ]
+  };
+
   if (isMobile) {
     return (
       <React.Fragment>
-        <header>
+        <header className="header-mobile">
           <img src={menuIcon} alt="Hawk menu" />
           <img src={hawkLogo} alt="Hawk logo" />
           <div className="user-session">
@@ -28,10 +55,19 @@ export default function Header() {
     );
   }
   return (
-    <header>
-      <img src={menuIcon} alt="Hawk menu" />
+    <header className="header-desktop">
       <img src={hawkLogo} alt="Hawk logo" />
+      <ul>
+        {state.links.map((link) => (
+          <li key={link.id}>
+            <Link to={link.path} className="">
+              {link.text}
+            </Link>
+          </li>
+        ))}
+      </ul>
       <div className="user-session">
+        <img src={searchIcon} alt="Hawk search" />
         <img src={profileIcon} alt="Hawk profile" />
         <img src={cartIcon} alt="Hawk cart" />
       </div>
