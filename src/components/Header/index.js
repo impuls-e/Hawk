b8bd1ac0b1ebbdd28f7646c2057650beb7b3fe67
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-04-02 17:39:24 -0300

    Add more style for mobile screen

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import './styles.css';
+import { isMobile } from 'react-device-detect';
 
-import menuIcon from '../../images/menu.svg';
-import hawkLogo from '../../images/hawk.svg';
-import profileIcon from '../../images/profile.svg';
-import cartIcon from '../../images/cart.svg';
+import menuIcon from '../../images/menuIcon.svg';
+import hawkLogo from '../../images/hawkIcon.svg';
+import profileIcon from '../../images/profileIcon.svg';
+import cartIcon from '../../images/cartIcon.svg';
+import searchIcon from '../../images/searchIcon.svg';
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <img src={menuIcon} alt="Hawk menu" />
-    <img src={hawkLogo} alt="Hawk logo" />
-    <div className="user-session">
-      <img src={profileIcon} alt="Hawk profile" />
-      <img src={cartIcon} alt="Hawk cart" />
-    </div>
-  </header>
-);
-
-export default Header;
+export default function Header() {
+  if (isMobile) {
+    return (
+      <React.Fragment>
+        <header>
+          <img src={menuIcon} alt="Hawk menu" />
+          <img src={hawkLogo} alt="Hawk logo" />
+          <div className="user-session">
+            <img src={profileIcon} alt="Hawk profile" />
+            <img src={cartIcon} alt="Hawk cart" />
+          </div>
+        </header>
+        <div className="search-area">
+          <p>O que você procura ?</p>
+          <img src={searchIcon} alt="Hawk search" />
+        </div>
+      </React.Fragment>
+    );
+  }
+  return (
+    <header>
+      <img src={menuIcon} alt="Hawk menu" />
+      <img src={hawkLogo} alt="Hawk logo" />
+      <div className="user-session">
+        <img src={profileIcon} alt="Hawk profile" />
+        <img src={cartIcon} alt="Hawk cart" />
+      </div>
+    </header>
+  );
+}
