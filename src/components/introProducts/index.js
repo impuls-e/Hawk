13c8aebc3000ca2479991983997263d89a62604f
viniciusmdias <vinicius-mdias@hotmail.com>
2020-04-15 19:11:54 -0300

    Create html file

--- src/components/introProducts/index.js
+++ src/components/introProducts/index.js
@@ -1,23 +1,55 @@
 import React from "react"
 import "./styles.css"
-import { FaExternalLinkAlt } from "react-icons/fa"
+import Img from "gatsby-image"
 
 export default function introProducts({ title, description, value, imgUrl }) {
+  console.log(imgUrl)
   return (
     <div className="intro-projects">
       <section>
-        <h4>Cliente</h4>
-        <h1>{title}</h1>
-        <h4>Sobre o Projeto</h4>
-        <p>{description}</p>
+        <div className="img-main">
+          <img
+            src={`http://localhost:8000/${imgUrl.childImageSharp.fluid.src}`}
+            alt="product image"
+          />
+        </div>
+        <div className="imgs-body">
+          <img
+            src={`http://localhost:8000/${imgUrl.childImageSharp.fluid.src}`}
+            alt="product image"
+          />
+          <img
+            src={`http://localhost:8000/${imgUrl.childImageSharp.fluid.src}`}
+            alt="product image"
+          />
+          <img
+            src={`http://localhost:8000/${imgUrl.childImageSharp.fluid.src}`}
+            alt="product image"
+          />
+        </div>
       </section>
       <aside>
-        <h4>Serviços</h4>
-        <p>{value}</p>
-        <h4>Website</h4>
-        <a className="site" href={imgUrl}>
-          Site {title} <FaExternalLinkAlt />
-        </a>
+        <div className="description">
+          <h1>{title}</h1>
+          <span>{value}</span>
+          <h2>Descrição</h2>
+          <p>{description}</p>
+        </div>
+        <div className="product-lenght">
+          <h2>Tamanho</h2>
+          <div className="lenght">
+            <a href="#">P</a>
+            <a href="#">M</a>
+            <a href="#">G</a>
+            <a href="#">GG</a>
+            <a href="#">EG</a>
+          </div>
+        </div>
+        <div className="product-footer">
+          <h2>Quantidade</h2>
+          <a href="#">1</a>
+          <a href="#">Comprar</a>
+        </div>
       </aside>
     </div>
   )
