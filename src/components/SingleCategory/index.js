691a7f86aec44b444c3fa63a7d04c3487aff0e15
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-04-16 12:39:46 -0300

    Crete page camisetas and css adjustment

--- src/components/SingleCategory/index.js
+++ src/components/SingleCategory/index.js
@@ -4,7 +4,7 @@ import './styles.css';
 
 export default function SingleCategory({ category }) {
   return (
-    <section className="section product-catalog">
+    <section className="header-offset product-catalog">
       <h2> CAMISETAS</h2>
       <div>
         <Product product={'tshirt'} />
