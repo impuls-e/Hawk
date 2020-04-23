12e3451a688916fd80bdf0159433d2571587877d
viniciusmdias <vinicius-mdias@hotmail.com>
2020-04-23 15:29:18 -0300

    Add a checkout button to product page

--- src/templates/ProductPage/index.js
+++ src/templates/ProductPage/index.js
@@ -1,42 +1,70 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+import React from "react"
+import { graphql } from "gatsby"
+import { FiShoppingBag } from "react-icons/fi"
 
-import SEO from '../../components/Seo'
-import ProductForm from '~/components/ProductForm'
-import {
-  Img,
-  Container,
-  TwoColumnGrid,
-  GridLeft,
-  GridRight,
-} from '~/utils/styles'
-import { ProductTitle, ProductDescription } from './styles'
+import SEO from "../../components/Seo"
+import ProductForm from "~/components/ProductForm"
+import { Img } from "~/utils/styles"
 
 const ProductPage = ({ data }) => {
   const product = data.shopifyProduct
   return (
     <>
       <SEO title={product.title} description={product.description} />
-      <Container>
-        <TwoColumnGrid>
-          <GridLeft>
-            {product.images.map(image => (
-              <Img
-                fluid={image.localFile.childImageSharp.fluid}
-                key={image.id}
-                alt={product.title}
-              />
-            ))}
-          </GridLeft>
-          <GridRight>
-            <ProductTitle>{product.title}</ProductTitle>
-            <ProductDescription
-              dangerouslySetInnerHTML={{ __html: product.descriptionHtml }}
+
+      <div className="intro-projects">
+        <section>
+          <div className="img-main">
+            <Img
+              fluid={product.images[0].localFile.childImageSharp.fluid}
+              alt="An image apresentation of current product"
             />
+          </div>
+          <div className="imgs-body">
+            <img
+              src={`http://localhost:8000/static/2e86485e9b61f0a92f09ac7e376751f4/c2da5/tshirt-black.png`}
+              alt="Imagem do produto auxiliar"
+            />
+            <img
+              src={`http://localhost:8000/static/2e86485e9b61f0a92f09ac7e376751f4/c2da5/tshirt-black.png`}
+              alt="Imagem do produto auxiliar2"
+            />
+            <img
+              src={`http://localhost:8000/static/2e86485e9b61f0a92f09ac7e376751f4/c2da5/tshirt-black.png`}
+              alt="Imagem do produto auxiliar3"
+            />
+          </div>
+        </section>
+        <aside>
+          <div className="description">
+            <h1>{product.title}</h1>
+            <h3>R$ {product.priceRange.minVariantPrice.amount},00</h3>
+            <h4>Descrição do Produto</h4>
+            <p>{product.description}</p>
+          </div>
+          <div className="product-lenght">
+            <h4>{product.options.name}</h4>
+            <div className="lenght">
+              <a href="#">{product.options.values[0]}</a>
+              <a href="#">{product.options.values[1]}</a>
+              <a href="#">{product.options.values[2]}</a>
+              <a href="#">{product.options.values[3]}</a>
+              <a href="#">{product.options.values[4]}</a>
+            </div>
+          </div>
+          <div className="product-footer">
+            <h4>Quantidade</h4>
+            <div className="quantity">
+              <a href="#">- 1 +</a>
+            </div>
+
             <ProductForm product={product} />
-          </GridRight>
-        </TwoColumnGrid>
-      </Container>
+            <a href="#">
+              Adicionar ao Carrinho <FiShoppingBag />
+            </a>
+          </div>
+        </aside>
+      </div>
     </>
   )
 }
