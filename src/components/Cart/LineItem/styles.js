754c6bf585ca6ca4bc06667b4d4cd908108562cc
viniciusmdias <vinicius-mdias@hotmail.com>
2020-04-27 17:12:33 -0300

    Fix cart page structure

--- src/components/Cart/LineItem/styles.js
+++ src/components/Cart/LineItem/styles.js
@@ -1,9 +1,38 @@
-import styled from '@emotion/styled'
+import styled from "@emotion/styled"
 
 export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
   flex-wrap: wrap;
   padding: 2rem 0 2rem 0;
+
+  section {
+    display: flex;
+    align-items: center;
+  }
+
+  img {
+    width: 200px;
+    height: 200px;
+  }
+  p {
+    margin: 1vh 0;
+  }
+  button {
+    display: flex;
+    margin: 2vh 0 5vh 0;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    width: 40vw;
+    height: 8vh;
+    border-radius: 50px;
+    background-color: #000;
+    color: #fff;
+    text-decoration: none;
+    border: 2px solid #000;
+    align-self: flex-start;
+    cursor: pointer;
+  }
 `
--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Image from 'gatsby-image'
-import styled from '@emotion/styled'
-import { Global, css } from '@emotion/core'
+import React from "react"
+import Image from "gatsby-image"
+import styled from "@emotion/styled"
+import { Global, css } from "@emotion/core"
 
 export const breakpoints = {
   s: 576,
@@ -40,14 +40,18 @@ export const Img = styled(Image)`
 
 export const Container = styled.div`
   margin: 0 auto;
-  max-width: 960px;
+  padding: 18vh 2vw;
+
+  @media (min-width: 1024px) {
+    padding: 18vh 10vw 0;
+  }
 `
 
 export const TwoColumnGrid = styled.div`
   display: grid;
   grid-template-columns: 1fr 2rem 1fr;
   grid-template-rows: 1auto;
-  grid-template-areas: 'left . right';
+  grid-template-areas: "left . right";
 
   @media (max-width: ${breakpoints.l}px) {
     display: block;
