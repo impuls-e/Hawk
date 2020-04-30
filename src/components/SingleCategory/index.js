4fc8ea2b41b837c858bf4dd38438a17b4f004cfc
viniciusmdias <vinicius-mdias@hotmail.com>
2020-04-30 16:08:48 -0300

    change animations container

--- src/components/SingleCategory/index.js
+++ src/components/SingleCategory/index.js
@@ -4,15 +4,14 @@ import './styles.css'
 
 export default function SingleCategory({ category, title }) {
   return (
-    <section
-      className="header-offset product-catalog"
-      data-sal="slide-up"
-      data-sal-delay="200"
-      data-sal-easing="ease"
-      data-sal-duration="1000"
-    >
+    <section className="header-offset product-catalog">
       <h2> {title}</h2>
-      <div>
+      <div
+        data-sal="slide-up"
+        data-sal-delay="200"
+        data-sal-easing="ease"
+        data-sal-duration="1000"
+      >
         <Product product={category} />
       </div>
     </section>
