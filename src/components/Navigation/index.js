2d7d078f83e628ec2b4936a7e8135c2fdeac9a1f
jbastos.ufsc <jbastos.ufsc@gmail.com>
2020-05-07 15:16:20 -0300

    Close menu hamburger after option clicked

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,57 +1,55 @@
-import React, { useContext, useState } from 'react'
-import { Link } from 'gatsby'
+import React, { useContext, useState } from 'react';
+import { Link } from 'gatsby';
 
-import reduce from 'lodash/reduce'
-import PropTypes from 'prop-types'
-import './styles.css'
-import StoreContext from '~/context/StoreContext'
-import { CartCounter, MenuLink } from './styles'
-import headerLogo from '../../images/hawkIcon.svg'
+import reduce from 'lodash/reduce';
+import PropTypes from 'prop-types';
+import './styles.css';
+import StoreContext from '~/context/StoreContext';
+import { CartCounter, MenuLink } from './styles';
+import headerLogo from '../../images/hawkIcon.svg';
 // import profileIcon from "../../images/profileIcon.svg"
-import { FiShoppingCart } from 'react-icons/fi'
-import searchIcon from '../../images/searchIcon.svg'
+import { FiShoppingCart } from 'react-icons/fi';
+import searchIcon from '../../images/searchIcon.svg';
 
 const useQuantity = () => {
-  const {
-    store: { checkout },
-  } = useContext(StoreContext)
-  const items = checkout ? checkout.lineItems : []
-  const total = reduce(items, (acc, item) => acc + item.quantity, 0)
-  return [total !== 0, total]
-}
+  const { store: { checkout } } = useContext(StoreContext);
+  const items = checkout ? checkout.lineItems : [];
+  const total = reduce(items, (acc, item) => acc + item.quantity, 0);
+  return [ total !== 0, total ];
+};
 
 const Navigation = () => {
-  const [hasItems, quantity] = useQuantity()
-  const [classOn, setClassOn] = useState('')
-  const [show, setShow] = useState(false)
+  const [ hasItems, quantity ] = useQuantity();
+  const [ classOn, setClassOn ] = useState('');
+  const [ show, setShow ] = useState(false);
   const menu = {
     links: [
       {
         id: 1,
         path: '/camisetas',
-        text: 'camisetas',
+        text: 'camisetas'
       },
       {
         id: 2,
         path: '/shorts',
-        text: 'shorts',
+        text: 'shorts'
       },
       {
         id: 3,
         path: '/calcas',
-        text: 'calças',
+        text: 'calças'
       },
       {
         id: 4,
         path: '/acessorios',
-        text: 'acessórios',
-      },
-    ],
-  }
+        text: 'acessórios'
+      }
+    ]
+  };
   function handleMenu() {
-    document.body.style.overflow = show ? 'initial' : 'hidden'
-    show ? setClassOn('') : setClassOn('on')
-    setShow(!show)
+    document.body.style.overflow = show ? 'initial' : 'hidden';
+    show ? setClassOn('') : setClassOn('on');
+    setShow(!show);
   }
   return (
     <header>
@@ -70,9 +68,11 @@ const Navigation = () => {
 
         <nav>
           <ul>
-            {menu.links.map(link => (
+            {menu.links.map((link) => (
               <li key={link.id}>
-                <Link to={link.path}>{link.text}</Link>
+                <Link to={link.path} onClick={handleMenu}>
+                  {link.text}
+                </Link>
               </li>
             ))}
           </ul>
@@ -93,15 +93,15 @@ const Navigation = () => {
         <img src={searchIcon} alt="Hawk search" />
       </div>
     </header>
-  )
-}
+  );
+};
 
 Navigation.propTypes = {
-  siteTitle: PropTypes.string,
-}
+  siteTitle: PropTypes.string
+};
 
 Navigation.defaultProps = {
-  siteTitle: ``,
-}
+  siteTitle: ``
+};
 
-export default Navigation
+export default Navigation;
