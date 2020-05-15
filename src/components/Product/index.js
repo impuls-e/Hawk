0333402ed731e232c69770ae68532e6602e40ec3
viniciusmdias <vinicius-mdias@hotmail.com>
2020-05-15 16:31:59 -0300

    Fix img responsive layout in all screens

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -28,7 +28,7 @@ export default function Product({ product }) {
               localFile {
                 childImageSharp {
                   fluid {
-                    src
+                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
                   }
                 }
               }
@@ -64,7 +64,7 @@ export default function Product({ product }) {
               localFile {
                 childImageSharp {
                   fluid {
-                    src
+                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
                   }
                 }
               }
@@ -100,7 +100,7 @@ export default function Product({ product }) {
               localFile {
                 childImageSharp {
                   fluid {
-                    src
+                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
                   }
                 }
               }
@@ -136,7 +136,7 @@ export default function Product({ product }) {
               localFile {
                 childImageSharp {
                   fluid {
-                    src
+                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
                   }
                 }
               }
